f1e7784171e09515e9eadfb9a0012dd440d6f114
Tim K. <Tim.kindt@hotmail.com>
2025-11-07 22:33:52 +0100

    Init commit: structure & gitignore

---
+++ config/example_settings.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  7 22:21:11 2025
+
+@author: Chex
+"""
+
---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  7 22:20:12 2025
+
+@author: Chex
+"""
+
